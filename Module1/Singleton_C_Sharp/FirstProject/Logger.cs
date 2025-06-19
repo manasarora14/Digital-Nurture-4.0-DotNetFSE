55a6d55842a905681920e22c8408d4d5558a2feb
manasarora14 <manas.arora142@gmail.com>
2025-06-20 00:43:57 +0530

    Singleton_CSharp

---
+++ Module1/Singleton_C_Sharp/FirstProject/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Logger
+{
+    private static readonly Logger instance = new Logger();
+
+    private Logger()
+    {
+        Console.WriteLine("Logger Initialized");
+    }
+
+    public static Logger GetInstance()
+    {
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("Log: " + message);
+    }
+}
---
+++ Module1/Singleton_C_Sharp/FirstProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("First message.");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("Second message.");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Singleton works: Only one instance exists.");
+        }
+        else
+        {
+            Console.WriteLine("Singleton failed: Different instances.");
+        }
+    }
+}
