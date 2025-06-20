14093256e9790f1bba2f88837fecd36e453d5976
manasarora14 <manas.arora142@gmail.com>
2025-06-20 18:22:34 +0530

    DSA Problem 1

---
+++ Module2_Data Structures and Algorithms/Inventory_Management_System/Inventory_Management_System/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Inventory
+{
+    private Dictionary<int, Product> products = new Dictionary<int, Product>();
+
+    // Add product
+    public void AddProduct(Product product)
+    {
+        if (!products.ContainsKey(product.ProductId))
+        {
+            products[product.ProductId] = product;
+            Console.WriteLine("Product added.");
+        }
+        else
+        {
+            Console.WriteLine("Product ID already exists.");
+        }
+    }
+
+    // Update product
+    public void UpdateProduct(int productId, string name, int quantity, double price)
+    {
+        if (products.ContainsKey(productId))
+        {
+            products[productId].ProductName = name;
+            products[productId].Quantity = quantity;
+            products[productId].Price = price;
+            Console.WriteLine("Product updated.");
+        }
+        else
+        {
+            Console.WriteLine("Product not found.");
+        }
+    }
+
+    // Delete product
+    public void DeleteProduct(int productId)
+    {
+        if (products.Remove(productId))
+        {
+            Console.WriteLine("Product deleted.");
+        }
+        else
+        {
+            Console.WriteLine("Product not found.");
+        }
+    }
+
+    // View all products
+    public void ViewInventory()
+    {
+        Console.WriteLine("Current Inventory:");
+        foreach (var p in products.Values)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ Module2_Data Structures and Algorithms/Inventory_Management_System/Inventory_Management_System/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Inventory inventory = new Inventory();
+
+        inventory.AddProduct(new Product(101, "Laptop", 10, 75000));
+        inventory.AddProduct(new Product(102, "Mouse", 50, 500));
+        inventory.ViewInventory();
+
+        inventory.UpdateProduct(102, "Wireless Mouse", 40, 650);
+        inventory.ViewInventory();
+
+        inventory.DeleteProduct(101);
+        inventory.ViewInventory();
+    }
+}
