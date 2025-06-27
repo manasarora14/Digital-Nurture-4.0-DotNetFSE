67a8b333bb573c1ce5df47ac88e32c7ee44a9dcf
manasarora14 <manas.arora142@gmail.com>
2025-06-28 02:00:33 +0530

    Nunit 1st Question

---
+++ Module4 Nunit and Moq/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
